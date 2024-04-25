773399c7d00168da346214e5c5508f3c0c68dd0e
Anurag-2043 <itsaworkspace2043@gmail.com>
2024-04-25 21:52:29 +0530

    proper files uploaded

--- project-4/app/src/App.jsx
+++ project-4/app/src/App.jsx
@@ -1,5 +1,15 @@
+import styled from "styled-components";
+
 const App = () => {
-  return <div>App</div>;
-};
+
+  return (
+    <MainContainer>
+      App
+      </MainContainer>
+  )
+}
 
 export default App;
+const MainContainer = styled.div`
+  
+`
